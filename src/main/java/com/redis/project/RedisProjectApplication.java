b7a8849b02999b90f1eb1cff49bea585409289c5
Prakhyat Singhal <singhalprakhyat2001@gmail.com>
2022-12-24 02:09:57 +0530

    Added Caching

--- src/main/java/com/redis/project/RedisProjectApplication.java
+++ src/main/java/com/redis/project/RedisProjectApplication.java
@@ -5,6 +5,9 @@ import com.redis.project.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +15,7 @@ import java.util.List;
 @SpringBootApplication
 @RestController
 @RequestMapping("/product")
+@EnableCaching
 public class RedisProjectApplication {
     @Autowired
     private ProductDao dao;
@@ -27,13 +31,17 @@ public class RedisProjectApplication {
     }
 
     @GetMapping("/{id}")
+    @Cacheable(key = "#id",value = "Product",unless = "#result.price > 1000")
     public Product findProduct(@PathVariable int id) {
         return dao.findProductById(id);
     }
+
     @DeleteMapping("/{id}")
-    public String remove(@PathVariable int id)   {
+    @CacheEvict(key = "#id",value = "Product")
+    public String remove(@PathVariable int id) {
         return dao.deleteProduct(id);
     }
+
 
 
     public static void main(String[] args) {
